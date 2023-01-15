4a6b8bbd000de0b3ea907edf244ea4da20463094
pedroygor <pedroygor2010@hotmail.com>
2023-01-15 04:22:01 -0300

    refactor: indentação

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
-import { Countdown } from "./components/Countdown"
-import { Header } from "./components/Header"
+import { Countdown } from './components/Countdown'
+import { Header } from './components/Header'
 import './index.css'
 
 export const App = () => {
-  return(
+  return (
     <div className="bg-blue-900 text-white-100 h-screen">
-    <div className="container mx-auto">
-      <Header />
-      <Countdown />
+      <div className="container mx-auto">
+        <Header />
+        <Countdown />
+      </div>
     </div>
-  </div>
-  
   )
 }
-
-
--- src/components/Countdown/index.tsx
+++ src/components/Countdown/index.tsx
@@ -1,6 +1,5 @@
 import { useState, useEffect, ChangeEvent } from 'react'
 
-
 export const Countdown = () => {
   const [minutes, setMinutes] = useState(0)
   const [seconds, setSeconds] = useState(0)
@@ -27,7 +26,7 @@ export const Countdown = () => {
     }
   }
   const handleStop = () => {
-    if(isRunning) {
+    if (isRunning) {
       setIsRunning(false)
     } else {
       setIsRunning(true)
@@ -42,10 +41,10 @@ export const Countdown = () => {
   }
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setInputNumber(event.target.value);
-    const parseNum = Number(event.target.value);
-    if(parseNum > 0) {
-      setMinutes(parseNum);
+    setInputNumber(event.target.value)
+    const parseNum = Number(event.target.value)
+    if (parseNum > 0) {
+      setMinutes(parseNum)
     } else {
       setMinutes(0)
     }
@@ -56,38 +55,51 @@ export const Countdown = () => {
 
   return (
     <div className="w-full md:w-2/5 h-96 mx-auto bg-blue-700 rounded-md border-b-8 border-b-red-300">
-  <div className="flex flex-col gap-5 items-center">
-    <h1 className="text-xl mt-5 leading-7 uppercase">Timer</h1>
-    <div className="flex gap-2 flex-col">
-      <label htmlFor="minutes">Quanto minutos você quer?</label>
-      <input
-        type="text"
-        className="text-white-500 border-b-2 border-b-green-100 outline-0 bg-blue-700 pl-1 focus:border-b-green-300 text-center"
-        onChange={handleChange}
-        value={inputNumber}
-      />
-    </div>
-    <div className='flex gap-5'>
-      <div className='md:w-28 flex flex-col gap-3 justify-center items-center'>
-        <p className='border bg-blue-700 w-24 h-14 text-4xl flex justify-center items-center rounded-md'>{minutesRemaining}</p>
-        <p>Minutos</p>
+      <div className="flex flex-col gap-5 items-center">
+        <h1 className="text-xl mt-5 leading-7 uppercase">Timer</h1>
+        <div className="flex gap-2 flex-col">
+          <label htmlFor="minutes">Quanto minutos você quer?</label>
+          <input
+            type="text"
+            className="text-white-500 border-b-2 border-b-green-100 outline-0 bg-blue-700 pl-1 focus:border-b-green-300 text-center"
+            onChange={handleChange}
+            value={inputNumber}
+          />
+        </div>
+        <div className="flex gap-5">
+          <div className="md:w-28 flex flex-col gap-3 justify-center items-center">
+            <p className="border bg-blue-700 w-24 h-14 text-4xl flex justify-center items-center rounded-md">
+              {minutesRemaining}
+            </p>
+            <p>Minutos</p>
+          </div>
+          <div className="md:w-28 flex flex-col gap-3 justify-center items-center">
+            <p className="border bg-blue-700 w-24 h-14 text-4xl flex justify-center items-center rounded-md">
+              {secondsRemaining < 10
+                ? `0${secondsRemaining}`
+                : secondsRemaining}
+            </p>
+            <p>Segundos</p>
+          </div>
+        </div>
+        <div className="flex gap-2">
+          <button
+            className="bg-green-300 w-full md:w-28 h-10 rounded-md uppercase text-lg transition-all duration-300 ease-in-out hover:bg-green-100 text-white-100"
+            onClick={handleStart}>
+            Start
+          </button>
+          <button
+            className="w-full md:w-28 h-10 rounded-md border-2 border-white-400 uppercase text-lg transition-all duration-300 ease-in-out hover:bg-blue-500 text-white-100"
+            onClick={handleStop}>
+            Pause
+          </button>
+          <button
+            className="bg-red-300 w-full md:w-28 h-10 rounded-md uppercase text-lg transition-all duration-300 ease-in-out hover:opacity-75 text-white-100"
+            onClick={handleReset}>
+            Reset
+          </button>
+        </div>
       </div>
-      <div className='md:w-28 flex flex-col gap-3 justify-center items-center'>
-        <p className='border bg-blue-700 w-24 h-14 text-4xl flex justify-center items-center rounded-md'>
-          {secondsRemaining < 10
-            ? `0${secondsRemaining}`
-            : secondsRemaining}
-        </p>
-        <p>Segundos</p>
-      </div>
     </div>
-    <div className='flex gap-2'>
-      <button className='bg-green-300 w-full md:w-28 h-10 rounded-md uppercase text-lg transition-all duration-300 ease-in-out hover:bg-green-100 text-white-100' onClick={handleStart}>Start</button>
-      <button className='w-full md:w-28 h-10 rounded-md border-2 border-white-400 uppercase text-lg transition-all duration-300 ease-in-out hover:bg-blue-500 text-white-100' onClick={handleStop}>Pause</button>
-      <button className='bg-red-300 w-full md:w-28 h-10 rounded-md uppercase text-lg transition-all duration-300 ease-in-out hover:opacity-75 text-white-100' onClick={handleReset}>Reset</button>
-    </div>
-  </div>
-</div>
-
   )
 }
